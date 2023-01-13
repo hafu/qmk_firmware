ce5b7e9c94e521509ce5081f561dedeae8c76fb3
Hannes Fuchs <hannes.fuchs@gmx.org>
2023-01-13 22:57:10 +0100

    gmmk/pro/iso/keymap/hafu: Refactor keymap
    Make the keymap match more to the defalts of 65% layouts

--- keyboards/gmmk/pro/iso/keymaps/hafu/keymap.c
+++ keyboards/gmmk/pro/iso/keymaps/hafu/keymap.c
@@ -112,10 +112,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     // Notable change is Caps Lock -> Left Control
     [_BASE] = LAYOUT(
         KC_ESC,     KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,   KC_F12,  KC_PSCR,          KC_MUTE,
-        KC_GRV,     KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS,  KC_EQL,  KC_BSPC,          TD(TD_INS_SFTINS),
-        KC_TAB,     KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC,  KC_RBRC,                   TD(TD_PGUP_HOME),
-        MO(_FN1),   KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,  KC_NUHS, KC_ENT,           TD(TD_PGDN_END),
-        KC_LSFT,    KC_NUBS, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,           KC_RSFT, KC_UP,   TD(TD_DEL_SFTDEL),
+        KC_GRV,     KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS,  KC_EQL,  KC_BSPC,          KC_HOME,
+        KC_TAB,     KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC,  KC_RBRC,                   KC_PGUP,
+        MO(_FN1),   KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,  KC_NUHS, KC_ENT,           KC_PGDN,
+        KC_LSFT,    KC_NUBS, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,           KC_RSFT, KC_UP,   KC_END,
         KC_LCTL,    KC_LGUI, KC_LALT,                            KC_SPC,                             KC_RALT, MO(_FN0), KC_RCTL, KC_LEFT, KC_DOWN, KC_RGHT
     ),
     // Gaming layer, only difference is that default keycode for Caps Lock key (used by CP77 for example)
@@ -136,12 +136,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         _______, _______, _______,                            _______,                            _______, _______, _______, _______, _______, _______
     ),
     [_FN0] = LAYOUT(
-        _______, _______, _______, KC_SLEP, KC_PWR,  _______, _______, KC_MPRV, KC_MNXT, KC_MPLY, KC_MUTE, KC_VOLD, KC_VOLU, RGB_TOG,          KC_CST_TGL_RTRY_FNC,
-        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,   RESET,          KC_BRK,
-        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, RGB_HUI, RGB_SAI, RGB_SPI,                   KC_HOME,
-        _______, _______, _______, _______, _______, TG(_GAMING), _______, _______, _______, _______, RGB_HUD, RGB_SAD, RGB_SPD, _______,      KC_END,
-        _______, _______, _______, _______, _______, _______, _______, TG(_NUM), _______, _______, _______, _______,          _______, RGB_VAI, _______,
-        _______, _______, _______,                            _______,                            _______, _______, _______, RGB_RMOD, RGB_VAD, RGB_MOD
+        _______, _______, _______, KC_SLEP, KC_PWR,  _______,     _______, KC_MPRV,  KC_MNXT, KC_MPLY, KC_MUTE, KC_VOLD, KC_VOLU, RGB_TOG,           KC_CST_TGL_RTRY_FNC,
+        _______, _______, _______, _______, _______, _______,     _______, _______,  _______, _______, _______, _______, _______,  KC_DEL,           KC_INS,
+        _______, _______, _______, _______, _______, _______,     _______, _______,  _______, _______, RGB_HUI, RGB_SAI, RGB_SPI,                    KC_VOLU,
+        _______, _______, _______, _______, _______, TG(_GAMING), _______, _______,  _______, _______, RGB_HUD, RGB_SAD, RGB_SPD, QK_BOOT,           KC_VOLD,
+        _______, _______, _______, _______, _______, _______,     _______, TG(_NUM), _______, _______, _______, _______,          _______,  RGB_VAI, _______,
+        _______, _______, _______,                                _______,                             _______, _______, _______, RGB_RMOD, RGB_VAD, RGB_MOD
     ),
     [_FN1] = LAYOUT(
         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______,
